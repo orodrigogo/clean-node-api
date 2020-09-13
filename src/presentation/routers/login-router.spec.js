31cf02ddb36f1b1d88065f691752fa89cc8ab2f5
rodrigo <rodrigo.rgtic@gmail.com>
2020-09-13 08:53:31 -0300

    style: eslint + lint-staged

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,24 +1,23 @@
 class LoginRouter {
-  route (httpRequest){
-    if(!httpRequest.body.email){
+  route(httpRequest) {
+    if (!httpRequest.body.email) {
       return {
-        statusCode: 400
-      }
+        statusCode: 400,
+      };
     }
   }
 }
 
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
-    const sut = new LoginRouter()
+    const sut = new LoginRouter();
     const httpRequest = {
       body: {
-        password: 'any_password'
-      }
-    }
+        password: 'any_password',
+      },
+    };
 
-    const httpResponse = sut.route(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-
-  })
-})
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});

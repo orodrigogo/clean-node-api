368efa3c6762d747869228e94ddcf4b903e79d13
rodrigo <rodrigo.rgtic@gmail.com>
2020-09-13 11:43:10 -0300

    move LoginRouter to its own file

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,18 @@
+const MissingParamError = require('./missing-param-error');
+
+class HttpResponse {
+  static badRequest(paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName),
+    };
+  }
+
+  static serverError() {
+    return {
+      statusCode: 500,
+    };
+  }
+}
+
+module.exports = HttpResponse;

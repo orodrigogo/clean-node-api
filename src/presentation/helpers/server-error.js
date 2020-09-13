32fcbe9635343d54c7ab3c97b1cba18b6d6449ca
rodrigo <rodrigo.rgtic@gmail.com>
2020-09-13 15:14:25 -0300

    move server error to a helper method

---
+++ src/presentation/helpers/server-error.js
@@ -0,0 +1,8 @@
+class ServerError extends Error {
+  constructor() {
+    super('Internal Error');
+    this.name = 'ServerError';
+  }
+}
+
+module.exports = ServerError;
--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,9 +2,10 @@
 const LoginRouter = require('./login-router');
 const MissingParamError = require('../helpers/missing-param-error');
 const UnauthorizedError = require('../helpers/unauthorized-error');
+const ServerError = require('../helpers/server-error');
 
 // Design Pattern Factory.
-const makeSut = () => {
+const makeAuthUseCase = () => {
   // Mock para capturar valor para comparar.
   class AuthUseCaseSpy {
     auth(email, password) {
@@ -15,8 +16,12 @@ const makeSut = () => {
     }
   }
 
+  return new AuthUseCaseSpy();
+};
+
+const makeSut = () => {
   // Dependency Injector
-  const authUseCaseSpy = new AuthUseCaseSpy();
+  const authUseCaseSpy = makeAuthUseCase();
   authUseCaseSpy.accesToken = 'valid_token';
 
   // Set default token case route not set acess token.
@@ -27,6 +32,16 @@ const makeSut = () => {
   return {
     sut, authUseCaseSpy,
   };
+};
+
+const makeAuthUseCaseWithError = () => {
+  class AuthUseCaseSpy {
+    auth() {
+      throw new Error();
+    }
+  }
+
+  return new AuthUseCaseSpy();
 };
 
 describe('Login Router', () => {
@@ -65,6 +80,7 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route();
     expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 
   test('Should return 500 if no httpRequest has no body', () => {
@@ -72,6 +88,7 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route({});
     expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 
   test('Should call AuthUseCase with corret params', () => {
@@ -138,10 +155,28 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 
   test('Should return 500 if no AuthUseCase has no auth method', () => {
     const sut = new LoginRouter({});
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('Should return 500 if AuthUseCase throws', () => {
+    const authUseCaseSpy = makeAuthUseCaseWithError();
+    // authUseCaseSpy.accesToken = 'valid_token';
+    const sut = new LoginRouter(authUseCaseSpy);
 
     const httpRequest = {
       body: {
